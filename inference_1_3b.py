4f3ac56e09fbe1350725e884e096cbf75282d117
saria-lh <139324475+saria-lh@users.noreply.github.com>
2024-03-16 01:44:04 -0400

    Update inference_1_3b.py

--- inference_1_3b.py
+++ inference_1_3b.py
@@ -19,10 +19,9 @@
 
 import torch
 from transformers import AutoModelForCausalLM
-
 from deepseek_vl.models import MultiModalityCausalLM, VLChatProcessor
 from deepseek_vl.utils.io import load_pil_images
-
+from read_vid import *
 # specify the path to the model
 model_path = "deepseek-ai/deepseek-vl-1.3b-chat"
 vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
@@ -36,33 +35,45 @@ vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
 conversation = [
     {
         "role": "User",
-        "content": "<image_placeholder>Describe this image briefly.",
-        "images": ["./images/training_pipelines.jpg"],
+        "content": "<image_placeholder> Describe this image, OBJECTS list briefly, and the text in it briefly.",
     },
     {"role": "Assistant", "content": ""},
 ]
 
 
 # load images and prepare for inputs
-pil_images = load_pil_images(conversation)
-prepare_inputs = vl_chat_processor(
-    conversations=conversation, images=pil_images, force_batchify=True
-).to(vl_gpt.device)
 
-# run image encoder to get the image embeddings
-inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+path = './vid.mp4'
+path_without_extension = path.rsplit('.', 1)[0]
+txt_file = path_without_extension + '.txt'
+selected_frames = read_video_frames_with_exact_timestamps(path)
+frames_only = [f[1] for f in selected_frames]
+save_images_to_folder(frames_only, path_without_extension+'_images')
+for i, pil_im in enumerate(selected_frames):
+    ts, im = pil_im
+    prepare_inputs = vl_chat_processor(
+        conversations=conversation, images=[im], force_batchify=True
+    ).to(vl_gpt.device)
 
-# run the model to get the response
-outputs = vl_gpt.language_model.generate(
-    inputs_embeds=inputs_embeds,
-    attention_mask=prepare_inputs.attention_mask,
-    pad_token_id=tokenizer.eos_token_id,
-    bos_token_id=tokenizer.bos_token_id,
-    eos_token_id=tokenizer.eos_token_id,
-    max_new_tokens=512,
-    do_sample=False,
-    use_cache=True,
-)
+    # run image encoder to get the image embeddings
+    inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
 
-answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
-print(f"{prepare_inputs['sft_format'][0]}", answer)
+    # run the model to get the response
+    outputs = vl_gpt.language_model.generate(
+        inputs_embeds=inputs_embeds,
+        attention_mask=prepare_inputs.attention_mask,
+        pad_token_id=tokenizer.eos_token_id,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
+        max_new_tokens=256,
+        do_sample=False,
+        use_cache=True,
+    )
+
+    answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+    print(f"{prepare_inputs['sft_format'][0]}", answer)
+    with open(txt_file, 'a') as f:
+        f.write(f'TS: {ts}\n')
+        f.write(answer)
+        f.write('\n\n')
+
